a6077afa996494d67981880b8fa5e13e17e4c503
alaminone <alaminone00@gmail.com>
2023-12-25 02:27:30 +0600

    create navbar

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div className="flex flex-col justify-center items-center">
-        <img src={ErrorJpg} width={700} height={700} alt="" />
+        <img src={'https://i.ibb.co/vmsDBYt/Na-Nov-26.jpg'} width={700} height={700} alt="" />
   
         <Link to={'/'}>
           <button className="bg-darkOne text-lightOne px-12 py-4 text-sm md:text-xl lg:text-2xl font-lora font-bold tracking-wide rounded-lg border-2 border-lightOne">
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom';
 
 
 import React from 'react';
+import Navbar from './sheard/nav/Navbar';
 
 const Layout = () => {
     return (
         <div>
+            <Navbar></Navbar>
                <Outlet />
         </div>
     );
